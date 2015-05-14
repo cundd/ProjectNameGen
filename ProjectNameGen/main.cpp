c89dfec9e6462e6ad9d7e2eeccac64041e7eba2f
Daniel Corn <d@cundd.net>
2015-05-14 10:53:45 +0200

    Initial Commit

---
+++ ProjectNameGen/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ProjectNameGen
+//
+//  Created by Daniel Corn on 14.05.15.
+//  Copyright (c) 2015 cundd. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
